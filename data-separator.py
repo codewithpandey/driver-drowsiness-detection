11610b5dd496e83f75a686fe490977f2c8a6d6ec
codewithpandey <abhishekap2001@gmail.com>
2022-03-21 06:24:37 +0530

    --no-commit

--- data-separator.py
+++ data-separator.py
@@ -14,23 +14,23 @@ if not os.path.isdir("eye-data/train"):
     os.makedirs("eye-data/test/closed")
     os.makedirs("eye-data/test/open")
 
-for img in random.sample(os.listdir("eye-data/closed"), 700):
-    img = os.path.join("eye-data/closed", img)
+for img in random.sample(os.listdir("archive/closed"), 700):
+    img = os.path.join("archive/closed", img)
     shutil.copy(img, "eye-data/train/closed")
-for img in random.sample(os.listdir("eye-data/open"), 700):
-    img = os.path.join("eye-data/open", img)
+for img in random.sample(os.listdir("archive/open"), 700):
+    img = os.path.join("archive/open", img)
     shutil.copy(img, "eye-data/train/open")
-for img in random.sample(os.listdir("eye-data/closed"), 350):
-    img = os.path.join("eye-data/closed", img)
+for img in random.sample(os.listdir("archive/closed"), 350):
+    img = os.path.join("archive/closed", img)
     shutil.copy(img, "eye-data/valid/closed")
-for img in random.sample(os.listdir("eye-data/open"), 350):
-    img = os.path.join("eye-data/open", img)
+for img in random.sample(os.listdir("archive/open"), 350):
+    img = os.path.join("archive/open", img)
     shutil.copy(img, "eye-data/valid/open")
-for img in random.sample(os.listdir("eye-data/closed"), 175):
-    img = os.path.join("eye-data/closed", img)
+for img in random.sample(os.listdir("archive/closed"), 175):
+    img = os.path.join("archive/closed", img)
     shutil.copy(img, "eye-data/test/closed")
-for img in random.sample(os.listdir("eye-data/open"), 175):
-    img = os.path.join("eye-data/open", img)
+for img in random.sample(os.listdir("archive/open"), 175):
+    img = os.path.join("archive/open", img)
     shutil.copy(img, "eye-data/test/open")
 
 # %%
@@ -42,22 +42,22 @@ if not os.path.isdir("yawn-data/train"):
     os.makedirs("yawn-data/test/no_yawn")
     os.makedirs("yawn-data/test/yawn")
 
-for img in random.sample(os.listdir("yawn-data/no_yawn"), 700):
-    img = os.path.join("yawn-data/no_yawn", img)
+for img in random.sample(os.listdir("archive/no_yawn"), 700):
+    img = os.path.join("archive/no_yawn", img)
     shutil.copy(img, "yawn-data/train/no_yawn")
-for img in random.sample(os.listdir("yawn-data/yawn"), 700):
-    img = os.path.join("yawn-data/yawn", img)
+for img in random.sample(os.listdir("archive/yawn"), 700):
+    img = os.path.join("archive/yawn", img)
     shutil.copy(img, "yawn-data/train/yawn")
-for img in random.sample(os.listdir("yawn-data/no_yawn"), 350):
-    img = os.path.join("yawn-data/no_yawn", img)
+for img in random.sample(os.listdir("archive/no_yawn"), 350):
+    img = os.path.join("archive/no_yawn", img)
     shutil.copy(img, "yawn-data/valid/no_yawn")
-for img in random.sample(os.listdir("yawn-data/yawn"), 350):
-    img = os.path.join("yawn-data/yawn", img)
+for img in random.sample(os.listdir("archive/yawn"), 350):
+    img = os.path.join("archive/yawn", img)
     shutil.copy(img, "yawn-data/valid/yawn")
-for img in random.sample(os.listdir("yawn-data/no_yawn"), 175):
-    img = os.path.join("yawn-data/no_yawn", img)
+for img in random.sample(os.listdir("archive/no_yawn"), 175):
+    img = os.path.join("archive/no_yawn", img)
     shutil.copy(img, "yawn-data/test/no_yawn")
-for img in random.sample(os.listdir("yawn-data/yawn"), 175):
-    img = os.path.join("yawn-data/yawn", img)
+for img in random.sample(os.listdir("archive/yawn"), 175):
+    img = os.path.join("archive/yawn", img)
     shutil.copy(img, "yawn-data/test/yawn")
 # %%
